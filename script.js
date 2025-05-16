1bc2587d2bcf0b82407e1834f9d646d02534ffbe
Edison Rincón C. <113222675+EdisonRincon@users.noreply.github.com>
2025-05-16 15:56:04 -0500

    script

---
+++ script.js
@@ -0,0 +1,13 @@
+function validarformulario(){
+    let nombre = document.getElementById("nombre").value;
+    let email = document.getElementById("email").value;
+    let imagen = document.getElementById("imagen").value;
+    if(nombre== "" || email== "" || imagen== ""){
+        alert("Todos los campos son obligatorios");
+        return false;
+    }
+}
+function mostrarImagen(event){
+    let imagen=document.getElementById("ver-imagen");
+    imagen.src=URL.createObjectURL(event.target.files[0]);
+}
